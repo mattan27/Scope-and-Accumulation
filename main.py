6c1827c161fc23c3c8afe3771d18888235b40561
Matthew-JJ3 <98533759+mattan27@users.noreply.github.com>
2022-02-24 13:59:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+def multiply(arg1=5, arg2=0):
+  accumulator = 0
+  for i in range(arg2):
+	  accumulator = accumulator + arg1
+  return accumulator
+
+def exponent(arg1=5, arg2=7):
+  accumulator = 1
+  for i in range(arg2):
+    accumulator = accumulator * arg1
+  return accumulator  
+
+  def square(arg=0):
+    return multiply(arg1=arg, arg2=arg)
+
+result = multiply(arg1=5, arg2=7)
+print(result)
+
+result = exponent(arg1=2, arg2=1)
+print(result)
+
+result = square(arg=2)
+print(result)
